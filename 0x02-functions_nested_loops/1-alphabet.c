628f5c543a47db6878ac074d520aba27eea66f70
Berenice-uwimpuhwe <uberenice16@gmail.com>
2022-04-12 04:10:38 -0700

    Printing the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Description: Program to print alphabet in lower case
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+
+	_putchar('\n');
+
+	return (0);
+}
+void print_alphabet(void)
+{
+	for (i = 'a'; i = 'z'; i++)
+		_putchar(i);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 0x02. C - Functions, nested loops
 Task 0. _putchar
+Task 1. alphabet game
