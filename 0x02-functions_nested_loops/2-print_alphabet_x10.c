954bfb5deb4329cef14160abfddb542b7c9bc6ab
Berenice-uwimpuhwe <uberenice16@gmail.com>
2022-04-12 04:40:36 -0700

    Printing 10 times the alphabet in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,13 +4,6 @@
  * print_alphabet_x10 - prints alphabet 10 times
  * Return: Always 0.
  */
-void print_alphabet_x10(void);
-
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
 void print_alphabet_x10(void)
 {
 	int i = 0;
