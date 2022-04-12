90e466e5346bec3f778884315efc51571aea432e
Berenice-uwimpuhwe <uberenice16@gmail.com>
2022-04-12 04:17:37 -0700

    Printing the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,20 +4,12 @@
  * Description: Program to print alphabet in lower case
  * Return: Always 0.
  */
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-
-	_putchar('\n');
-
-	return (0);
-}
 void print_alphabet(void)
 {
 	char i;
 
-	for (i = 'a'; i = 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
+
+	_putchar('\n');
 }
