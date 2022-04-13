41052d63ba618b21492dd706a5aea35a91844422
Berenice-uwimpuhwe <uberenice16@gmail.com>
2022-04-13 13:03:11 -0700

    Task 0 multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,14 @@
 #include "main.h"
-
 /**
  * main - tests function that prints if positive or negative
- * positive_or_negative - prints if number is positive or negative
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
 	
 	i = 0;
+
 	positive_or_negative(i);
 	
 	return (0);
