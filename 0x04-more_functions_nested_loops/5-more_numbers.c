61f040d24250e8ad01102142dd55eca623b7e14c
Berenice-uwimpuhwe <uberenice16@gmail.com>
2022-04-14 13:39:23 -0700

    Printing 0 to 14 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,22 +8,29 @@
 
 void more_numbers(void)
 {
-	int i, j, a;
+	int i = 0, j = 0, a, b;
 
-	for (a = 0; a < 10; a++)
+	while (i < 10)
 	{
-		for (i = 0; i <= 1; i++)
+		while (j <= 14)
 		{
-			for (j = 0; j <= 9; j++)
+			if (j < 10)
 			{
-				if (!(i == 1 && j == 5))
-				{
-					if (i)
-						_putchar(i + '0');
-					_putchar(j + '0');
-				}
+				b = j;
 			}
+			else
+			{
+				a = j / 10;
+				b = j % 10;
+				_putchar(a + '0');
+			}
+			_putchar(b + '0');
+			j++;
 		}
+		i++;
+
+		j = 0;
+
 		_putchar('\n');
 	}
 }
