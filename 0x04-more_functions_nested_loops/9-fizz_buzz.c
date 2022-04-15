ae9adcd484cb6110b7676d5c31bd09746f541b1f
Berenice-uwimpuhwe <uberenice16@gmail.com>
2022-04-15 13:50:19 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,15 @@ int main(void)
 
 	for (i = 0; i <= 100; i++)
 	{
-		if (i % 3 && !(i % 5))
+		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf("Fizz");
 		}
-		else if (i % 5 && !(i % 3))
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf("Buzz");
 		}
-		else if (i % 3 && i % 5)
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
