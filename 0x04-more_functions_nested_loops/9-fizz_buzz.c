6bbc99933828e8743a1818f6ada4a11a2a9d8d55
Berenice-uwimpuhwe <uberenice16@gmail.com>
2022-04-15 13:59:53 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * Description: Prints 1 to 100, fizz for 3x, buzz for 5x, fizzbuzz for 15x
+ * 
  * Return: Always 0
  */
 
@@ -12,15 +12,15 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (((i % 3) == 0) && ((i % 5) != 0))
+		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf("Fizz");
 		}
-		else if (((i % 5) == 0) && ((i % 3) != 0))
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf("Buzz");
 		}
-		else if (((i % 3) == 0) && ((i % 5) == 0))
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
