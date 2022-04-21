4bbe846d6874ff12075a50c6e65bb31e735da82c
Berenice-uwimpuhwe <uberenice16@gmail.com>
2022-04-21 13:37:40 -0700

    Resetting a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updating a pointer
+ * @n: takes in an integer
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+0x05. C - Pointers, arrays and strings
+Task 0. 98 Battery st.
