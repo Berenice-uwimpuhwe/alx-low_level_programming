15741372db52959f08fdca3fc306055d7dee5890
Berenice-uwimpuhwe <uberenice16@gmail.com>
2022-04-25 14:42:45 -0700

    Capitalizing whole string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,12 +11,12 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, j = 0;
+
 	while (dest[i] != '\0')
 		i++;
-	
 	while (src[j] != '\0' && n > j)
 	{
-		dest[i] = src [j];
+		dest[i] = src[j];
 		j++;
 		i++;
 	}
