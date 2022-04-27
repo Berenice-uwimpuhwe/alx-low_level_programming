da42869cedf2bda4afc82c4ae725394b5971dbd9
Berenice-uwimpuhwe <uberenice16@gmail.com>
2022-04-27 13:05:32 -0700

    Task strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,7 +9,7 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j, a_len = 0, len = 0;
-	
+
 	while (accept[a_len] != '\0')
 		a_len++;
 	for (i = 0; s[i] != '\0'; i++)
@@ -19,5 +19,5 @@ unsigned int _strspn(char *s, char *accept)
 			else
 				if (j == a_len - 1)
 					goto exit;
-exit : return (len);
+exit: return (len);
 }
