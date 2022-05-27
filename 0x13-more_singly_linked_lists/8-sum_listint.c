65e7a3962cc59b37f1c19eb50346d5cc8670df12
Berenice-uwimpuhwe <uberenice16@gmail.com>
2022-05-27 10:49:39 -0700

    Task 8 correction

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_listint - makes the sum of all the data (n) 
+ * sum_listint - makes the sum of all the data (n)
  * of a listint_t linked list
  * @head: head
  * Return: sum of all the data (n) of a listint_t linked list
