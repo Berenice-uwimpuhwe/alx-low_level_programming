940fae09b8b813d02268f76491fe117e691ebb32
Berenice-uwimpuhwe <uberenice16@gmail.com>
2022-06-01 10:52:47 -0700

    Task 0 reading and printing txt file to stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,15 +1,11 @@
 #include "main.h"
-#include <unistd.h>
-#include <stdio.h>
 
 /**
- * read_textfile - reads a text file and prints it to the standard output
+ * read_textfile - reads a text file and
+ * prints it to the standard output
  * @filename: name of the file
  * @letters: number of letters it should read and print
- * Return: actual number of letters it could read and print,
- * if the file can not be opened or read, return 0
- * if filename is NULL return 0
- * if write fails or does not write the expected amount of bytes, return 0
+ * Return: actual number of letters it could read and print, 0 if error
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
